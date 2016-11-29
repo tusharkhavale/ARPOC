6d2b3e26b6365355b166274c173ae85e4f14d078
tusharkhavale <tushar2717@gmail.com>
2016-11-29 10:08:00 +0530

    First Commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Collections.Generic;
+
+public class CharacterController : MonoBehaviour {
+
+	Animation anim;
+
+	void Start()
+	{
+		anim = transform.GetComponent<Animation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (anim.isPlaying)
+			return;
+
+		if (Input.touches.Length > 3)
+			anim.Play ("Death");
+		else if (Input.touches.Length > 2)
+			anim.Play ("Knockback");
+		else if (Input.touches.Length > 1)
+			anim.Play ("Attack");
+		else if (Input.touches.Length > 0)
+			anim.Play ("Skill");
+
+
+//		string test = string.Empty;
+//
+//		Dictionary <string,string> dict = new Dictionary<string, string> ();
+//
+//		dict.
+
+	}
+}
---
+++ Assets/Scripts/RetainRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RetainRotation : MonoBehaviour {
+
+	public GameObject m_player;
+	public Text m_log;
+
+
+//	void Update()
+//	{
+//		m_log.text = "Rotation of the player : " + m_player.transform.rotation;
+//	}
+}
